8ac71982490a3caaeaa8cef7e9911e52763f1369
Tyler Farrior <tylerfarrior@Tylers-Air.attlocal.net>
2023-07-19 18:33:36 -0400

    set up initial readme layout

--- script.js
+++ script.js
@@ -18,17 +18,15 @@ function generatePassword() {
   var charLower = "abcdefghijklmnopqrstuvwxyz".split("");
   var charNumber = "0123456789".split("");
   var charSpecial = "!@#$%^&*()_-+=".split("");
-  
   var passwordHat = []
   var password = "";
-  
+
   
   var passwordLength = prompt("How long would you like your password to be?(8-128)");
   if (passwordLength < 8 || passwordLength > 128) {
     alert("You must choose a length between 8 - 128. Please retry");
     generatePassword()
   }
-
   if(confirm("Do you want uppercase letters in your password?")) {
     passwordHat = passwordHat.concat(charUpper)
   } 
