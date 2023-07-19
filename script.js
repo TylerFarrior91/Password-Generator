c333fd3338ae75900a4e2d1feaf185b298bc8be3
Tyler Farrior <tylerfarrior@Tylers-Air.attlocal.net>
2023-07-19 18:24:41 -0400

    set up ability to create a password based on which characters are confirmed upon

--- script.js
+++ script.js
@@ -14,19 +14,41 @@ function writePassword() {
 
 // Generate a random password
 function generatePassword() {
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // Characters to include in the password
+  var charLower = "abcdefghijklmnopqrstuvwxyz".split("");
+  var charNumber = "0123456789".split("");
+  var charSpecial = "!@#$%^&*()_-+=".split("");
+  
+  var passwordHat = []
+  var password = "";
+  
+  
   var passwordLength = prompt("How long would you like your password to be?(8-128)");
   if (passwordLength < 8 || passwordLength > 128) {
     alert("You must choose a length between 8 - 128. Please retry");
     generatePassword()
   }
 
-  // Length of the password
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+="; // Characters to include in the password
-  var password = "";
+  if(confirm("Do you want uppercase letters in your password?")) {
+    passwordHat = passwordHat.concat(charUpper)
+  } 
+  if(confirm("Do you want lowercase letters in your password?")) {
+    passwordHat = passwordHat.concat(charLower)
+  }
+  if(confirm("Do you want numbers in your password?")) {
+    passwordHat = passwordHat.concat(charNumber)
+  }
+  if(confirm("Do you want special characters in your password?")) {
+    passwordHat = passwordHat.concat(charSpecial)
+  }
+  if(passwordHat.length === 0) {
+    alert("You need to choose atleast one type of character, to build a password. Please retry");
+    return "";
+  }
 
   for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
+    var randomIndex = Math.floor(Math.random() * passwordHat.length);
+    password += passwordHat[randomIndex];
   }
 
   return password;
